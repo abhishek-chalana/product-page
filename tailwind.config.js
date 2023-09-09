87d856cfa666a93818e355d00b0e6a28c25afca5
Abhishek.Chalana <abhishek.chalana@jptokyo.co.jp>
2023-09-09 14:33:33 +0530

    shop for jewellery

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bluecode': '#6497ce',
+      },
+    }
+  },
+  plugins: [],
+}
+
